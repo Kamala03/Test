a4b41703dd6f15c148e4cd4a8e2920ef195271e5
Kamala03 <bagirova212003@gmail.com>
2025-07-11 14:57:01 +0400

    change gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        PersonRegistration personRegistration = new PersonRegistration();
+
+        try {
+            personRegistration.registration();
+        } catch (MyException e) {
+            System.out.println("Something went wrong");
+            System.out.println("Try again");
+        }
 
     }
 }
